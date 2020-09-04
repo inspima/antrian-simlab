9b3b5c0a92d8946baab66ef56545efb2b7786c99
Nambi Sembilu <nambisembilu@gmail.com>
2020-09-05 03:23:09 +0700

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                {{date('Y')}} © Inspima <span class="text-muted hide-phone pull-right"></span>
+                {{date('Y')}} © ITD Unair <span class="text-muted hide-phone pull-right"> Developed by <a target="_blank" href="https://inspima.com">Inspima</a></span>
             </div>
         </div>
     </div>
