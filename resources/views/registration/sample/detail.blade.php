41903139a5e7f2422acf14bd502d7b7b5e9fc219
Nambi Sembilu <nambisembilu@gmail.com>
2021-02-07 23:32:31 +0700

    User Table Responsive

--- resources/views/registration/sample/detail.blade.php
+++ resources/views/registration/sample/detail.blade.php
@@ -36,7 +36,7 @@
                         <p class="text-muted m-b-30 font-14">{{session('org_name')}}</p>
                     </div>
                     <div class="form-group ">
-                        <label for="date" >Tanggal</label>
+                        <label for="date">Tanggal</label>
                         <p class="text-muted m-b-30 font-14">{{$data->date}}</p>
                     </div>
                     <div class="form-group">
@@ -54,8 +54,9 @@
                     <hr>
                     <h4 class="mt-0 header-title">Data Pasien</h4>
                     <hr>
-                    <table class="table table-striped dt-responsive nowrap table-vertical">
-                        <thead>
+                    <div class="table-responsive">
+                        <table class="table table-striped dt-responsive nowrap table-vertical">
+                            <thead>
                             <tr>
                                 <th>No</th>
                                 <th class="text-center">No. KTP</th>
@@ -67,40 +68,43 @@
                                 <th class="text-center">Tes Ke</th>
                                 <th class="text-center">Status Pemeriksaan</th>
                             </tr>
-                        </thead>
-                        <tbody>
+                            </thead>
+                            <tbody>
                             @foreach ($data->registration_patiens as $p)
-                            <tr>
-                                <td>{{$p->name}}</td>
-                                <td class="text-center">{{$p->id_number}}</td>
-                                <td class="text-center">{{Carbon\Carbon::parse($p->born_date)->translatedFormat('d F Y')}}</td>
-                                <td class="text-center">{{$p->age}} Tahun</td>
-                                <td class="text-center">{{$p->age}}</td>
-                                <td>{{$p->address}}</td>
-                                <td class="text-center">{{$p->mobile}}</td>
-                                <td class="text-center">{{$p->test_loop}}</td>
-                                <td class="text-center">
-                                    @if ($p->sync_status=='0')
-                                        <span class="badge badge-danger font-14">Belum Diproses</span>
-                                    @elseif ($p->sync_status=='1')
-                                        <span class="badge badge-info font-14">Proses Pengujian</span><br/>
-                                        Kode Reg. : <br/>
-                                        <b style="font-size: 0.9em">{{$p->simlab_reg_code}}</b>
-                                    @elseif ($p->sync_status=='2')
-                                        <span class="badge badge-success font-14">Sudah Selesai</span><br/>
-                                        Kode Reg. : <br/>
-                                        <b style="font-size: 0.9em">{{$p->simlab_reg_code}}</b><br/>
-                                        <a target="_blank" style="color: white" class="btn btn-sm btn-primary waves-effect waves-light" href="<?=route('registration.sample.print-result',base64_encode(md5($p->id))) ?>"><i class="fa fa-print"></i> Cetak Hasil</a>
-                                    @endif
-                                </td>
-                            </tr>
+                                <tr>
+                                    <td>{{$p->name}}</td>
+                                    <td class="text-center">{{$p->id_number}}</td>
+                                    <td class="text-center">{{Carbon\Carbon::parse($p->born_date)->translatedFormat('d F Y')}}</td>
+                                    <td class="text-center">{{$p->age}} Tahun</td>
+                                    <td class="text-center">{{$p->age}}</td>
+                                    <td>{{$p->address}}</td>
+                                    <td class="text-center">{{$p->mobile}}</td>
+                                    <td class="text-center">{{$p->test_loop}}</td>
+                                    <td class="text-center">
+                                        @if ($p->sync_status=='0')
+                                            <span class="badge badge-danger font-14">Belum Diproses</span>
+                                        @elseif ($p->sync_status=='1')
+                                            <span class="badge badge-info font-14">Proses Pengujian</span><br/>
+                                            Kode Reg. : <br/>
+                                            <b style="font-size: 0.9em">{{$p->simlab_reg_code}}</b>
+                                        @elseif ($p->sync_status=='2')
+                                            <span class="badge badge-success font-14">Sudah Selesai</span><br/>
+                                            Kode Reg. : <br/>
+                                            <b style="font-size: 0.9em">{{$p->simlab_reg_code}}</b><br/>
+                                            <a target="_blank" style="color: white" class="btn btn-sm btn-primary waves-effect waves-light" href="<?=route('registration.sample.print-result', base64_encode(md5($p->id))) ?>"><i class="fa fa-print"></i> Cetak Hasil</a>
+                                        @endif
+                                    </td>
+                                </tr>
                             @endforeach
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                     <hr>
                     <div class="row form-group">
                         <div class="col-sm-12">
-                            <button type="button" class="btn btn-blue-grey waves-effect waves-light" onclick="window.location.href ='{{route($route.'index')}}'"><i class="ion-arrow-left-b"></i> Kembali </button>
+                            <button type="button" class="btn btn-blue-grey waves-effect waves-light" onclick="window.location.href ='{{route($route.'index')}}'">
+                                <i class="ion-arrow-left-b"></i> Kembali
+                            </button>
                         </div>
                     </div>
                 </div>
