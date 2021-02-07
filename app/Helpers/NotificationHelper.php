2cf90322c495938cc9caf0bad2379294fe56a0d0
Nambi Sembilu <nambisembilu@gmail.com>
2021-02-07 14:54:45 +0700

    Ganti Provider Whatsapp

--- app/Helpers/NotificationHelper.php
+++ app/Helpers/NotificationHelper.php
@@ -50,25 +50,27 @@
                 $token = getenv('WA_PROVIDER_TOKEN');
                 $phone = $to_number;
                 $content = $this->formatingMessageWhatsapp($message);
-                $url = getenv('WA_PROVIDER_URL') . 'send-message.php';
-
-                $curl = curl_init();
-                curl_setopt($curl, CURLOPT_URL, $url);
-                curl_setopt($curl, CURLOPT_HEADER, 0);
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
-                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-                curl_setopt($curl, CURLOPT_TIMEOUT, 30);
-                curl_setopt($curl, CURLOPT_POST, 1);
-                curl_setopt($curl, CURLOPT_POSTFIELDS, array(
-                    'token' => $token,
+                $url = getenv('WA_PROVIDER_URL') . '/send-message';
+                $data =[
                     'phone' => $phone,
                     'message' => $content,
-                ));
+                ];
+                $curl = curl_init();
+                curl_setopt($curl, CURLOPT_HTTPHEADER,
+                    array(
+                        "Authorization: $token",
+                    )
+                );
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
                 $response = curl_exec($curl);
                 $response = !$response ? [] : json_decode($response);
                 curl_close($curl);
-                if ($response->result == 'true') {
+                if ($response->status) {
                     return [
                         'status' => 1,
                         'message' => "Success"
