bfbc4e160597e04a0397e8a0e735e4b369489ea4
Nambi Sembilu <nambisembilu@gmail.com>
2021-07-21 16:09:01 +0700

    Jam update operasional

--- app/Http/Controllers/Registration/RegistrationSampleController.php
+++ app/Http/Controllers/Registration/RegistrationSampleController.php
@@ -362,7 +362,7 @@
                 $data = [
                     'message' => "Pendaftaran anda diterima, silahkan datang pada [lb]" .
                         'Hari / Tanggal : ' . Carbon::parse($reg->queue_date)->translatedFormat('l, d F Y') . '[lb] ' .
-                        'Waktu : 09.00 -10.00 WIB [lb][lb]' .
+                        'Waktu : 08.00 -09.00 WIB [lb][lb]' .
                         'Daftar nama pasien pengirim sample [lb]' .
                         $list_patient_str . '[lb]' .
                         'Harap Print kemudian bawa bukti pendaftaran, dari link dibawah ini [lb][lb]' .
