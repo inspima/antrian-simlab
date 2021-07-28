5eaef904ccdcc83b3b4268ce842c79a2c31981eb
Nambi Sembilu <nambisembilu@gmail.com>
2021-07-28 15:13:08 +0700

    Penyesuaian Class

--- app/Http/Controllers/Registration/RegistrationSampleController.php
+++ app/Http/Controllers/Registration/RegistrationSampleController.php
@@ -3,6 +3,7 @@
     namespace App\Http\Controllers\Registration;
 
     use App\Helpers\NotificationHelper;
+    use App\Helpers\SimlabHelper;
     use App\Helpers\TextformattingHelper;
     use App\Http\Controllers\Controller;
     use App\Models\Master\Holiday;
@@ -34,64 +35,6 @@
         public function __construct()
         {
             $this->middleware('auth');
-        }
-
-        private function getRegistrasiSimlab($no_registrasi)
-        {
-            $query = "select rp.*,p.*,i.nama_instansi,d.nama_dokter,pt.nama_pasien_tipe,p.nama nama_pasien,p.alamat alamat_pasien,d.gelar_depan,d.gelar_belakang
-            from registrasi_pemeriksaan rp
-            join pasien p on rp.id_pasien=p.id_pasien
-            join instansi i on i.id_instansi=rp.id_instansi
-            left join dokter d on d.id_dokter=rp.id_dokter_pengirim
-            join pasien_tipe pt on pt.id_pasien_tipe =rp.id_pasien_tipe
-            where rp.no_registrasi='{$no_registrasi}'";
-            return collect(DB::connection('mysql-simlab')->select($query))->first();
-        }
-
-        private function getPasienPemeriksaanSimlab($no_registrasi)
-        {
-            $result = array();
-            $query = "
-            select pp.*,p.nilai_normal,p.flag_parent_group,p.nama_pengujian,p.kode_pengujian,s.nama_sample,peg.nama_pegawai nama_validator,rps.keterangan_sample
-            from pasien_pemeriksaan pp
-            join registrasi_pemeriksaan rp on rp.id_registrasi_pemeriksaan=pp.id_registrasi_pemeriksaan
-            join pengujian p on pp.id_pengujian=p.id_pengujian
-            left join registrasi_pasien_sample rps on rps.id_registrasi_pasien_sample=pp.id_registrasi_pasien_sample
-            left join pegawai peg on peg.id_pegawai = pp.id_petugas_validasi
-            left join sample s on s.id_sample = rps.id_sample
-            where rp.no_registrasi='{$no_registrasi}'
-            order by pp.id_pasien_pemeriksaan
-            ";
-            $data_pasien_pemeriksaan = DB::connection('mysql-simlab')->select($query);
-            foreach ($data_pasien_pemeriksaan as $index => $d) {
-                if ($d->flag_parent_group == '1') {
-                    $query_child = "
-
-
-				select p.*,p.id_pengujian as id_pengujian_child,pph.*,pph.keterangan as keterangan_hasil
-				from pengujian p
-                join pasien_pemeriksaan_hasil pph on pph.id_pengujian = p.id_pengujian and pph.id_registrasi_pemeriksaan='{$d->id_registrasi_pemeriksaan}'
-                where p.id_pengujian_group ='{$d->id_pengujian}'";
-                    $data_pengujian_child = DB::connection('mysql-simlab')->select($query_child);
-
-                    //$data_pengujian_child = array();
-                } else {
-                    $data_pengujian_child = array();
-                }
-                $query_sample_pasien = "
-                select rps.*,s.nama_sample,ps.id_pemeriksaan_sample
-                from registrasi_pasien_sample rps
-                join sample s on rps.id_sample=s.id_sample
-                left join pemeriksaan_sample ps on ps.id_registrasi_pasien_sample =rps.id_registrasi_pasien_sample and ps.id_pasien_pemeriksaan='{$d->id_pasien_pemeriksaan}'
-                where rps.id_registrasi_pemeriksaan='{$d->id_registrasi_pemeriksaan}'
-                order by rps.waktu_masuk desc,s.nama_sample
-                ";
-                $data_sample = DB::connection('mysql-simlab')->select($query_sample_pasien);
-                $data_pasien_pemeriksaan[$index]->data_sample = $data_sample;
-                $data_pasien_pemeriksaan[$index]->data_child = $data_pengujian_child;
-            }
-            $result = $data_pasien_pemeriksaan;
-            return $result;
         }
 
 
@@ -175,8 +118,8 @@
             $id = base64_decode($key);
             $reg_patien = RegistrationPatient::whereRaw('md5(id)=?', [$id])->first();
             $reg = Registration::find($reg_patien->registration_id);
-            $reg_simlab = $this->getRegistrasiSimlab($reg_patien->simlab_reg_code);
-            $reg_patiens_simlabs = $this->getPasienPemeriksaanSimlab($reg_patien->simlab_reg_code);
+            $reg_simlab = SimlabHelper::getRegistrasiSimlab($reg_patien->simlab_reg_code);
+            $reg_patiens_simlabs = SimlabHelper::getPasienPemeriksaanSimlab($reg_patien->simlab_reg_code);
             $user_pj = collect(DB::connection('mysql-simlab')->select("select * from pegawai where id_pegawai='73'"))->first();
             $params = [
                 "reg" => $reg,
